71658840b51b51d2b736ed8a6b92404086904fc8
SPECTR <ivanov8465@mail.ru>
2023-03-13 21:02:02 +0300

    Исправил 1 задание

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -6,7 +6,7 @@ if(number!.Length == 5)
 else Console.Write("Вы ошиблись!\nВведите пятизначное число!");
 void Palindrom(string number)
 {
-  if (number[0]==number[4] || number[1]==number[3]){
+  if (number[0]==number[4] && number[1]==number[3]){
     Console.WriteLine($"Число: {number} - палиндром.");
   }
   else Console.WriteLine($"Число: {number} - не является палиндромом.");
